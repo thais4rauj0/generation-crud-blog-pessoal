629a15a2ea99b9e9608342f0db11d22c5839554c
Thais <rm96185@fiap.com.br>
2022-10-03 11:04:22 -0300

    Update - Criando métodos get - pesquisa por titulo e id

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,25 +3,41 @@ package com.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
-@RestController
-@RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController /*Define como Controller*/
+@RequestMapping("/postagens") /*Endpoint*/
+@CrossOrigin(origins = "*", allowedHeaders = "*") /*Libera as routes para serem acessadas por outras plataformas*/
 public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
 	
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){
-		return ResponseEntity.ok(postagemRepository.findAll());
+	public ResponseEntity<List<Postagem>> getAll(){ /*Buscar postagens*/
+		return ResponseEntity.ok(postagemRepository.findAll()); /*Traz os métodos*/
+	}
+	
+	/*Comunicação com SQL*/
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){ /*Procurando a postagem por ID*/
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta)) /*Trazendo a postagem pelo ID*/
+			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		/*Lambda -> método dentro de outro método que não exige uma estrutura ou parâmetro, é um método mais resumido*/	
+	} 
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,6 +3,7 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -10,6 +11,9 @@ import com.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
+	/*Realizando uma busca específica por titulo*/
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo); /*Buscando tudo da tabela título*/
+	
 	List<Postagem> findAll();
 
 	
